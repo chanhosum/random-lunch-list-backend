620907550c44540cc82f4e718270a4deabef7a89
chanhosum <chanhosum1@gmail.com>
2019-01-06 07:43:25 +0800

    added cors

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var mongourl = 'mongodb://anson:password01@ds249824.mlab.com:49824/random-lunch-list';
 var assert = require('assert');
+
+var cors = require('cors')
+app.use(cors());
  
 app.get('/getFullList', function (req, res) {
 	MongoClient.connect(mongourl, function(err, database) {
