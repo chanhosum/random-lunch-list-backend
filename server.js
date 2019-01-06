9db0d9696a5cc7435337267c5e713cb4f93a12eb
chanhosum <chanhosum1@gmail.com>
2019-01-06 23:55:22 +0800

    added updateFullList API

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var mongourl = 'mongodb://anson:password01@ds249824.mlab.com:49824/random-lunch-list';
 var assert = require('assert');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 var cors = require('cors')
 app.use(cors());
@@ -38,6 +40,29 @@ app.get('/getUser', function (req, res) {
             	res.send(returnObject);
             }
 		});
+	});
+});
+
+app.post('/updateFullList', jsonParser, function (req, res) {
+	MongoClient.connect(mongourl, function(err, database) {
+		const myDB = database.db('random-lunch-list');
+		assert.equal(err, null);
+		var obj = req.body;
+		var counter = 0;
+		console.log(obj);
+		for (var key in obj) {
+			for(var i in obj[key]){
+				console.log(obj[key][i]);
+				myDB.collection("restaurant").update({name:obj[key][i]},{$set:{"properties":key,"order":i}}, function(err, result) {
+					assert.equal(err, null);
+					counter++;
+					if(counter==obj["fullListLength"]){
+						console.log(counter);
+						res.send("ok");
+					}
+				});
+			}
+	    }
 	});
 });
  
